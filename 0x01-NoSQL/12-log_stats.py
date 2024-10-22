b27b450c60dd07fd1abe8c85ce67bc78ecfa3227
Bz14 <bezueyerusalem@gmail.com>
2024-10-22 11:31:49 +0300

    Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""  Log stats """
+
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    total = logs.count_documents({})
+    print(f"{total} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    filter_path = {"method": "GET", "path": "/status"}
+    count = logs.count_documents(filter_path)
+    print(f"{count} status check")
+    client.close()
