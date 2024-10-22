5a8cb8996d820fd767a5161a5fc74e1a9dd30890
Bz14 <bezueyerusalem@gmail.com>
2024-10-22 11:48:15 +0300

    Files updated.

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Top students """
+
+
+def top_students(mongo_collection):
+    """ Top students """
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,32 +1,31 @@
 #!/usr/bin/env python3
-""" 12. Log stats
-"""
-
-
+""" Log stats """
 from pymongo import MongoClient
 
 
 def log_stats():
-    """ log_stats.
-    """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
+    """ Log stats """
+    client = MongoClient()
+    db = client.logs
+    nginx_collection = db.nginx
+
+    log_count = nginx_collection.count_documents({})
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
+    for method in methods:
+        method_counts[method] = nginx_collection.count_documents(
+            {"method": method})
+
+    status_check_count = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
-    print(f"{total} logs")
+
+    # Display results
+    print(f"{log_count} logs")
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+    for method in methods:
+        print(f"\tmethod {method}: {method_counts[method]}")
+    print(f"{status_check_count} status check")
 
 
 if __name__ == "__main__":
