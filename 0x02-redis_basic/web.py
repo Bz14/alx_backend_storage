5d2db4abdbe6ec7bbf67b147d31e6aca75b4d221
Bz14 <bezueyerusalem@gmail.com>
2024-10-23 15:08:52 +0300

    Files updated.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 """Web module"""
 
-import requests
 import redis
+import requests
+from functools import wraps
 from typing import Callable
-import functools
+
 
 r = redis.Redis()
 
 
 def data_cache(method: Callable) -> Callable:
     """Decorator that stores the return value of a method"""
-    @functools.wraps(method)
-    def wrapper(*args, **kwargs) -> str:
-        key = f"count:{args}"
-        r.incr(key)
-        value = r.get(key)
+    @wraps(method)
+    def invoker(url) -> str:
+        """ Invoker function """
+        r.incr(f"count:{url}")
+        value = r.get(f"count:{url}")
         if value:
             return value.decode('utf-8')
-        value = method(*args, **kwargs)
-        r.set(key, 0)
-        r.setx(key, 10, value)
+        value = method(url)
+        r.set(f"count:{url}", 0)
+        r.setx(f"count:{url}", 10, value)
         return value
-    return wrapper
-
+    return invoker
 
 
 @data_cache
