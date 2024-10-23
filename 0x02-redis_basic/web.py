920df4b2f3c91561d97d8fe6c198b9ab807edc93
Bz14 <bezueyerusalem@gmail.com>
2024-10-23 15:01:52 +0300

    Files updated.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,18 +3,30 @@
 
 import requests
 import redis
+from typing import Callable
+import functools
 
 r = redis.Redis()
 
 
+def data_cache(method: Callable) -> Callable:
+    """Decorator that stores the return value of a method"""
+    @functools.wraps(method)
+    def wrapper(*args, **kwargs) -> str:
+        key = f"count:{args}"
+        r.incr(key)
+        value = r.get(key)
+        if value:
+            return value.decode('utf-8')
+        value = method(*args, **kwargs)
+        r.set(key, 0)
+        r.setx(key, 10, value)
+        return value
+    return wrapper
+
+
+
+@data_cache
 def get_page(url: str) -> str:
     """Get the HTML content of a particular URL and returns it"""
-    key = f"count:{url}"
-    count = r.get(key)
-    if count:
-        r.incr(key)
-        return r.get(url)
-    else:
-        r.set(key, 1)
-        r.expire(key, 10)
-        return requests.get(url).text
+    return requests.get(url).text
